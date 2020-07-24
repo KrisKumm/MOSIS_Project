6ea37d5d9156fa809e7100ce4446528b44d80325
KrisKumm <kristijanilic997@gmail.com>
2020-07-24 11:38:33 +0200

    start1

---
+++ app/src/main/java/rs/elfak/mosis/kristijan/heavenguide/ui/login/LoggedInUserView.java
@@ -0,0 +1,17 @@
+package rs.elfak.mosis.kristijan.heavenguide.ui.login;
+
+/**
+ * Class exposing authenticated user details to the UI.
+ */
+class LoggedInUserView {
+    private String displayName;
+    //... other data fields that may be accessible to the UI
+
+    LoggedInUserView(String displayName) {
+        this.displayName = displayName;
+    }
+
+    String getDisplayName() {
+        return displayName;
+    }
+}
---
+++ app/src/main/java/rs/elfak/mosis/kristijan/heavenguide/ui/login/LoginViewModelFactory.java
@@ -0,0 +1,26 @@
+package rs.elfak.mosis.kristijan.heavenguide.ui.login;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.annotation.NonNull;
+
+import rs.elfak.mosis.kristijan.heavenguide.data.LoginDataSource;
+import rs.elfak.mosis.kristijan.heavenguide.data.LoginRepository;
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+public class LoginViewModelFactory implements ViewModelProvider.Factory {
+
+    @NonNull
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(LoginViewModel.class)) {
+            return (T) new LoginViewModel(LoginRepository.getInstance(new LoginDataSource()));
+        } else {
+            throw new IllegalArgumentException("Unknown ViewModel class");
+        }
+    }
+}
