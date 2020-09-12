b0e2d5817c2277a7d5590b6b44c92c096743e1a4
KrisKumm <kristijanilic997@gmail.com>
2020-09-12 16:00:11 +0200

    dizajn 1

--- app/src/main/java/rs/elfak/mosis/kristijan/heavenguide/SettingsActivity.java
+++ app/src/main/java/rs/elfak/mosis/kristijan/heavenguide/SettingsActivity.java
@@ -68,9 +68,6 @@ public class SettingsActivity extends AppCompatActivity {
                     default:
                         break;
                 }
-//                if(settingsTheme.isChecked()){
-//
-//                }
             }
         });
     }
